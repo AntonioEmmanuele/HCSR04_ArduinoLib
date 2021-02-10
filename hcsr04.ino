bc63035d3aa563b1da1e84ae02cf7d91bc262a3a
AntonioEmmanuele <58343836+AntonioEmmanuele@users.noreply.github.com>
2021-02-10 19:23:15 +0100

    Add files via upload

--- hcsr04.ino
+++ hcsr04.ino
@@ -11,8 +11,8 @@
     limitations under the License.
 */
 #include "hcsr04.hpp"
-#define SINGLE_TST 1
-#define ALL_IN_ONE_TST 0
+#define SINGLE_TST 0
+#define ALL_IN_ONE_TST 1
 #define PRINT_STATUS 0
 #define PRINT_SINGLE 1
 using namespace hcsr_04;
@@ -30,14 +30,21 @@ void setup() {
 void loop() {
 #if SINGLE_TST && !(ALL_IN_ONE_TST)
   my_ultra.hcsr04Trigger();
-  my_ultra.hcsr04EchoPoll();
-#elif ALL_IN_ONE_TST && !SINGLE_TST
-  error_poll=my_ultra.hcsr04AllInOne();
+  error_poll=my_ultra.hcsr04EchoPoll();
   if(HCSR04_IS_ERR(error_poll))
-    my_ultra.hcsr04PrintErr(HCSR04_TO_DIAGN(error_poll));
+      //my_ultra.hcsr04PrintErr(HCSR04_TO_DIAGN(error_get));
+  my_ultra.hcsr04DelayNextMeasurement();
+#elif ALL_IN_ONE_TST && !SINGLE_TST
+  error_poll=my_ultra.hcsr04AllInOne(ALLINONE_TODELAY);
+  if(HCSR04_IS_ERR(error_poll))
+   my_ultra.hcsr04PrintErr(HCSR04_TO_DIAGN(error_poll));
 #endif
 
 #if !PRINT_STATUS && PRINT_SINGLE
+if(my_ultra.hcsr04CheckOOR()){
+  Serial.println("[ ERROR ]Out of range \n");
+}
+else{
   time_echo=my_ultra.hcsr04GetMicros();
   error_get=my_ultra.hcsr04GetDistance(distance_cm,GET_CM);
   if(HCSR04_IS_ERR(error_get))
@@ -53,10 +60,8 @@ void loop() {
     Serial.print("Time:  ");
     Serial.println(time_echo);
     Serial.println("****************************");
+}
 #elif PRINT_STATUS && !PRINT_SINGLE
   my_ultra.Print(1);
 #endif
-  
-  
-  
 }
